c11ae64e3781092d4f855e892a52699e23b1d743
Dmitry Krasnyansky <k.dimonych@gmail.com>
2020-11-13 20:07:43 +0200

    Unit tests module was added to the project

---
+++ unit_tests/cppmath_matrix_test.cpp
@@ -0,0 +1,44 @@
+#include <istream>
+
+#include "cppmath_matrix.hpp"
+
+#define ASSERT_THROW( condition )                                   \
+{                                                                   \
+  if( !( condition ) )                                              \
+  {                                                                 \
+    throw std::runtime_error(   std::string( __FILE__ )             \
+                              + std::string( ":" )                  \
+                              + std::to_string( __LINE__ )          \
+                              + std::string( " in " )               \
+                              + std::string( __PRETTY_FUNCTION__ )  \
+    );                                                              \
+  }                                                                 \
+}
+
+#define ASSERT_EQUAL( x, y )                                        \
+{                                                                   \
+  if( ( x ) != ( y ) )                                              \
+  {                                                                 \
+    throw std::runtime_error(   std::string( __FILE__ )             \
+                              + std::string( ":" )                  \
+                              + std::to_string( __LINE__ )          \
+                              + std::string( " in " )               \
+                              + std::string( __PRETTY_FUNCTION__ )  \
+                              + std::string( ": " )                 \
+                              + std::to_string( ( x ) )             \
+                              + std::string( " != " )               \
+                              + std::to_string( ( y ) )             \
+    );                                                              \
+  }                                                                 \
+}
+
+void testBasic()
+{
+
+}
+
+int main(int, char**)
+{
+  testBasic();
+  return 0;
+}
