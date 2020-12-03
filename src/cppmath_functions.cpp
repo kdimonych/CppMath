3aaf57776f373373065bb40ad5f31d379f037bd4
Dmitry Krasnyansky <k.dimonych@gmail.com>
2020-12-03 16:42:06 +0200

    The constructor to initialize a Matrix from а two-dimensional array was implemented.

---
+++ src/cppmath_functions.cpp
@@ -0,0 +1,8 @@
+//
+//  cppmath_functions.cpp
+//  CppMath
+//
+//  Created by Dimonych on 20.11.20.
+//
+
+#include "cppmath_functions.hpp"
---
+++ src/cppmath_functions.hpp
@@ -0,0 +1,13 @@
+//
+//  cppmath_functions.hpp
+//  CppMath
+//
+//  Created by Dimonych on 20.11.20.
+//
+
+#ifndef cppmath_functions_hpp
+#define cppmath_functions_hpp
+
+#include <stdio.h>
+
+#endif /* cppmath_functions_hpp */
